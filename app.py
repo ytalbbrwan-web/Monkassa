cdb0539981b6d68edb52b9f67453f3b3d238b1f5
ytalbbrwan-web <ytalbbrwan@gmail.com>
2026-02-14 23:49:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,7 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Monkassa AI is running 🚀"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
