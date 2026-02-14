3cfa26376bfc6aa1e939c88f0c7d61c472b029b9
ytalbbrwan-web <ytalbbrwan@gmail.com>
2026-02-15 00:26:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# الصفحة الرئيسية
 @app.route("/")
 def home():
     return "Monkassa AI is running 🚀"
 
-# هنا الذكاء الاصطناعي
 @app.route("/ask", methods=["POST"])
 def ask():
     data = request.get_json()
@@ -16,9 +13,7 @@ def ask():
 
     reply = "انت قلت: " + message
 
-    return jsonify({
-        "response": reply
-    })
+    return jsonify({"response": reply})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
