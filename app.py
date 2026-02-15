133e7a3cd222ad3922bedcc19cab564cc4d93ed2
ytalbbrwan-web <ytalbbrwan@gmail.com>
2026-02-15 21:02:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-import os
-import requests
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
 
 TOKEN = os.environ.get("BOT_TOKEN")
-URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
+URL = f"https://api.telegram.org/bot{TOKEN}"
 
 @app.route("/")
 def home():
-    return "Bot is running!"
+    return "Bot is running"
 
-
-@app.route(f"/{TOKEN}", methods=["POST"])
+@app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
 
@@ -21,33 +19,15 @@ def webhook():
         chat_id = data["message"]["chat"]["id"]
         text = data["message"].get("text", "")
 
-        reply = handle_message(text)
+        reply = "وصلتني رسالتك 👍"
 
-        requests.post(URL, json={
+        requests.post(f"{URL}/sendMessage", json={
             "chat_id": chat_id,
             "text": reply
         })
 
     return "ok"
 
-
-def handle_message(text):
-    text = text.lower()
-
-    if "مرحبا" in text:
-        return "أهلا بك 👋"
-
-    elif "من انت" in text:
-        return "أنا بوت Monkassa 🤖"
-
-    elif "منتج" in text:
-        return "قريبا سنعرض أحدث الأحذية 🔥"
-
-    else:
-        return "لم أفهم سؤالك 🤔"
-
-
-# مهم جدا ل Render
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
